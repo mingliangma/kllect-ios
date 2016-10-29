c2b9fb4ab21ba738a7b19b3da3ad42f06043ccc6
Christopher Primerano <cprimera@mac.com>
2016-10-28 21:28:46 -0400

    Add start of interests view controller, grabs interests from API, displays them in CollectionViewController and allows selection and deselection of them

---
+++ kllect/Class/VC/InterestsViewController.swift
@@ -0,0 +1,96 @@
+//
+//  InterestsViewController.swift
+//  kllect
+//
+//  Created by Christopher Primerano on 2016-10-28.
+//  Copyright © 2016 Kllect. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+
+class InterestsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+	@IBOutlet private weak var headerLabel: UILabel!
+	@IBOutlet private weak var collectionView: UICollectionView!
+	@IBOutlet private weak var continueButton: UIButton!
+	@IBOutlet private weak var remainingLabel: UILabel!
+	
+	private var tags = [Tag]() {
+		didSet {
+			if let collectionView = self.collectionView {
+				DispatchQueue.main.async {
+					collectionView.reloadData()
+				}
+			}
+		}
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		self.getTags()
+		self.collectionView.allowsMultipleSelection = true
+	}
+	
+	func getTags() {
+		let task = URLSession.shared.dataTask(with: URL(string: "http://api.app.kllect.com/tags")!) { data, response, error in
+			if error == nil {
+				do {
+					let jsonData = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [AnyObject]
+					let tags = Mapper<Tag>().mapArray(JSONObject: jsonData)!
+					self.tags = tags
+				} catch {
+					// handle error
+				}
+			}
+		}
+		task.resume()
+	}
+	
+	func numberOfSections(in collectionView: UICollectionView) -> Int {
+		return 1
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return self.tags.count
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InterestCollectionCell", for: indexPath)
+		
+		let layer = cell.contentView.layer
+		layer.borderColor = UIColor.black.cgColor
+		layer.borderWidth = 0.5
+		layer.cornerRadius = 21
+		
+		let label = UILabel()
+		label.text = self.tags[indexPath.row].tagName.replacingOccurrences(of: "_", with: " ").capitalized
+		label.font = UIFont(name: "Colfax-Regular", size: 18)
+		label.frame = UIEdgeInsetsInsetRect(cell.contentView.bounds, UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+		label.sizeToFit()
+		
+		cell.contentView.addSubview(label)
+		
+		return cell
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+		let cell = collectionView.cellForItem(at: indexPath)
+		cell?.contentView.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+		let cell = collectionView.cellForItem(at: indexPath)
+		cell?.contentView.backgroundColor = UIColor.clear
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		let label = UILabel()
+		label.text = self.tags[indexPath.row].tagName.replacingOccurrences(of: "_", with: " ").capitalized
+		label.font = UIFont(name: "Colfax-Regular", size: 18)
+		label.sizeToFit()
+		let size = CGSize(width: label.bounds.size.width + 20, height: label.bounds.size.height + 20)
+		print(size)
+		return size
+	}
+
+}
