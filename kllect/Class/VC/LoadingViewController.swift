c43176df4a393e76d9556c9458f8374ac45558b9
Christopher Primerano <cprimera@mac.com>
2016-10-28 20:23:49 -0400

    Add basic loading view controller with progress bar and timer to transition to video list screen

---
+++ kllect/Class/VC/LoadingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoadingViewController.swift
+//  kllect
+//
+//  Created by Christopher Primerano on 2016-10-28.
+//  Copyright © 2016 Kllect. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+	@IBOutlet private weak var headerLabel: UILabel!
+	@IBOutlet private weak var progressBar: UIProgressView!
+	
+	private var progressTimer: Timer?
+	
+	override func viewDidAppear(_ animated: Bool) {
+		self.progressTimer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(LoadingViewController.updateProgressBar(sender:)), userInfo: nil, repeats: true)
+//		self.progressTimer = Timer(timeInterval: 0.05, target: self, selector: #selector(LoadingViewController.updateProgressBar), userInfo: nil, repeats: true)
+	}
+	
+	func updateProgressBar(sender: Timer) {
+		guard self.progressBar.progress < 1 else {
+			self.progressTimer?.invalidate()
+			self.progressTimer = nil
+			self.transitionToHomeScreen()
+			return
+		}
+		
+		self.progressBar.setProgress(self.progressBar.progress + 0.01, animated: true)
+	}
+	
+	func transitionToHomeScreen() {
+		self.performSegue(withIdentifier: "ToHomeScreen", sender: self)
+	}
+
+}
