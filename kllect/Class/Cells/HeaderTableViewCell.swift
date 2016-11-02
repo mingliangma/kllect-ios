046f153f2ed8f248bc10f0ae4c4b3a86774d55cc
Christopher Primerano <cprimera@mac.com>
2016-11-01 20:43:42 -0400

    Move HeaderTableViewCell and VideoTableViewCell

---
+++ kllect/Class/Cells/HeaderTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HeaderTableViewCell.swift
+//  kllect
+//
+//  Created by Christopher Primerano on 2016-10-19.
+//  Copyright © 2016 topmobile. All rights reserved.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+	
+	@IBOutlet weak var titleLabel: UILabel!
+
+}
